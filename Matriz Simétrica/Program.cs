402a543e36dcdda565cfe2f541a3bfc64af4bea0
lcsmssilva <126531330+lcsmssilva@users.noreply.github.com>
2023-08-28 18:38:07 -0300

    Add files via upload

---
+++ Matriz Simétrica/Program.cs
@@ -0,0 +1,46 @@
+﻿using System;
+class Program
+{
+    static void Main(string[] args)
+    {
+        string Linha = Console.ReadLine();
+        string[] Valores = Linha.Split(' ');
+
+        int M = int.Parse(Valores[0]);
+        int N = int.Parse(Valores[1]);
+
+        string[] Val;
+        string Linhas;
+
+        int[,] L = new int[M,N];
+
+        for (int i = 0; i < N; i++)
+        {
+            Linhas = Console.ReadLine();
+            Val = Linhas.Split(' ');
+
+            for (int j = 0; j < M; j++)
+            {
+            L[i,j] = int.Parse(Val[j]);
+            }
+        }
+
+        int true = 1;
+        int false = 0;
+
+        for(int i = 0; i<M; i++)
+        {
+            for(int j = 0; j<N; j++)
+            {
+                if(L[i,j] == L[j,i])
+                {
+                    true++;
+                }
+                else
+                {
+                    Console.WriteLine(false);
+                }
+            }
+        }
+    }
+}
