fb50f3eb1c93130aec9714722837d3bb87ee9ef8
lcsmssilva <126531330+lcsmssilva@users.noreply.github.com>
2023-08-28 21:58:31 -0300

    Update Program.cs

--- Matriz Simétrica/Program.cs
+++ Matriz Simétrica/Program.cs
@@ -1,46 +1,55 @@
-﻿using System;
+using System;
 class Program
 {
+    static void ConfereSimetria(int[,] Matriz, int linha, int coluna)
+    {
+        int Simetrica = 1;
+
+        for(int i = 0; i < linha; i++)
+        {
+            for(int j = 0; j < coluna; j++)
+            {    
+                if(Matriz[i,j] != Matriz[j,i])
+                {
+                    Simetrica = 0;
+                }
+            }
+        }
+        
+        Console.WriteLine(Simetrica);
+    }
     static void Main(string[] args)
     {
+        int Simetrica = 0;
+
         string Linha = Console.ReadLine();
         string[] Valores = Linha.Split(' ');
 
         int M = int.Parse(Valores[0]);
         int N = int.Parse(Valores[1]);
 
-        string[] Val;
-        string Linhas;
-
-        int[,] L = new int[M,N];
-
-        for (int i = 0; i < N; i++)
+        if(M != N)
         {
-            Linhas = Console.ReadLine();
-            Val = Linhas.Split(' ');
-
-            for (int j = 0; j < M; j++)
-            {
-            L[i,j] = int.Parse(Val[j]);
-            }
+            Console.WriteLine(Simetrica);
         }
-
-        int true = 1;
-        int false = 0;
-
-        for(int i = 0; i<M; i++)
+        else
         {
-            for(int j = 0; j<N; j++)
+            string[] Val;
+            string Linhas;
+
+            int[,] L = new int[M,N];
+
+            for (int i = 0; i < M; i++)
             {
-                if(L[i,j] == L[j,i])
+                Linhas = Console.ReadLine();
+                Val = Linhas.Split(' ');
+
+                for (int j = 0; j < N; j++)
                 {
-                    true++;
-                }
-                else
-                {
-                    Console.WriteLine(false);
+                L[i,j] = int.Parse(Val[j]);
                 }
             }
+            ConfereSimetria(L, M, N);
         }
     }
 }
